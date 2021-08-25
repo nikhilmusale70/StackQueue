76aec995854812a90e270a49fd20b095a75a3d88
Nikhil Musale <nikhilmusale70@gmail.com>
2021-08-25 10:48:35 +0530

    [Nikhil] - added peak, pop method for stack

--- src/MyLinkedList.java
+++ src/MyLinkedList.java
@@ -1,5 +1,6 @@
 public class MyLinkedList {
     Node head;
+    int totalSize;
 
     private void add(int key){
         Node node = new Node();
@@ -20,5 +21,63 @@ public class MyLinkedList {
     public void push(int key){
         add(key);
 
+    }
+
+    public void peak(){
+        Node n =head;
+        Node n2 = head;
+        int n1 ;
+        while (n.next.next != null){
+            if (n2.key > n2.next.key) {
+                n1 = n2.next.key;
+                n2.next.key = n2.key;
+                n2.key = n1;
+            }
+            n2=n2.next;
+
+            n=n.next;
+        }
+        if (n2.key > n2.next.key) {
+            n1 = n2.next.key;
+            n2.next.key = n2.key;
+            n2.key = n1;
+        }
+
+        System.out.println("Largest element is "+n2.next.key);
+    }
+
+    public int totalSize(){
+        Node n = head;
+        totalSize =0;
+
+        while (n.next != null){
+            n = n.next;
+            totalSize++;
+        }
+        return totalSize;
+    }
+
+    public void pop(){
+        Node n = head;
+        Node n1 = null;
+        totalSize();
+
+        for(int i=0;i<totalSize-1;i++)
+        {
+            n = n.next;
+        }
+        System.out.println("Poped out :- " + n.next.key);
+
+        n1 = n.next;
+        n.next = n1.next;
+        n1 = null;
+    }
+
+    public void show(){
+        Node n = head;
+        while (n != null) {
+            System.out.println(n.key);
+            n=n.next;
+        }
     }
 }
--- src/StackQueueRunner.java
+++ src/StackQueueRunner.java
@@ -4,6 +4,16 @@ public class StackQueueRunner {
         linkedList.push(56);
         linkedList.push(30);
         linkedList.push(70);
-        
+        linkedList.show();
+        System.out.println();
+
+
+        linkedList.peak();
+        System.out.println();
+
+        linkedList.pop();
+        linkedList.pop();
+        linkedList.show();
+
     }
 }
